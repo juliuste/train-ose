a380c2aa932b2989831af0a89021e794e88f6b85
Julius Tens <mail@juliustens.eu>
2019-06-23 16:13:57 +0200

    adapt to fpti-js: stations -> stations.all

--- test.js
+++ test.js
@@ -3,49 +3,59 @@
 const tapeWithoutPromise = require('tape')
 const addPromiseSupport = require('tape-promise').default
 const tape = addPromiseSupport(tapeWithoutPromise)
-const validate = require('validate-fptf')
+const validate = require('validate-fptf')()
 const isBoolean = require('lodash/isBoolean')
 const isNumber = require('lodash/isNumber')
 const moment = require('moment-timezone')
+const fptiTests = require('fpti-tests')
+const getStream = require('get-stream').array
+
 const trainOSE = require('.')
+const pkg = require('./package.json')
 
-tape('train-ose.stations & train-ose.edges', async (t) => {
-	const s = await trainOSE.stations()
-	t.ok(s.length > 30, 'stations length')
+tape('train-ose fpti tests', async t => {
+	await t.doesNotReject(fptiTests.packageJson(pkg), 'valid package.json')
+	t.doesNotThrow(() => fptiTests.packageExports(trainOSE, ['stations.all', 'journeys']), 'valid module exports')
+	t.doesNotThrow(() => fptiTests.stationsAllFeatures(trainOSE.stations.all.features, []), 'valid stations.all features')
+})
 
-	for (let station of s) {
-		validate(station)
+tape('train-ose.stations & train-ose.edges', async t => {
+	const stations = await getStream(trainOSE.stations.all())
+	t.ok(stations.length > 30, 'stations length')
+
+	// base-check all stations
+	for (let station of stations) {
+		t.doesNotThrow(() => validate(station), 'valid fptf')
 		t.ok(station.location.country.length === 2, 'station location country')
 		t.ok(station.location.timezone.length > 8, 'station location timezone')
 		t.ok(station.location.timezone.indexOf('Europe/') === 0, 'station location timezone')
-
 		t.ok(isBoolean(station.active), 'station active')
 	}
 
-	const athens = s.find(x => x.id === 'ΑΘΗΝ')
+	// deep-check athens station
+	const athens = stations.find(x => x.id === 'ΑΘΗΝ')
 	t.ok(athens.name === 'Αθήνα', 'athens name')
 	t.ok(athens.nameEnglish === 'Athens', 'athens nameEnglish')
 	t.ok(athens.location.country === 'GR', 'athens location country')
 	t.ok(athens.location.timezone === 'Europe/Athens', 'athens location timezone')
 	t.ok(athens.active === true, 'athens active')
 
-	const e = await trainOSE.edges()
-	t.ok(e.length > 30, 'edges length')
+	// check edges
+	const edges = await trainOSE.edges()
+	t.ok(edges.length > 30, 'edges length')
 
-	for (let edge of e) {
+	for (let edge of edges) {
 		t.ok(edge.source, 'edge source')
-		const source = s.find(x => x.id === edge.source)
+		const source = stations.find(x => x.id === edge.source)
 		t.ok(source, 'edge source')
 
 		t.ok(edge.target, 'edge target')
-		const target = s.find(x => x.id === edge.target)
+		const target = stations.find(x => x.id === edge.target)
 		t.ok(target, 'edge target')
 
 		t.ok(isNumber(edge.distance), 'edge distance')
 		t.ok(edge.distance > 0, 'edge distance')
 	}
-
-	t.end()
 })
 
 tape('train-ose.journeys', async (t) => {
